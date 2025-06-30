fc618203be5d9beb33344066d0a96873c68af7a4
Marco Paredes <marcop_93_04@hotmail.com>
2025-06-30 10:56:49 -0500

    prueba url

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3500;
+const PORT = process.env.PORT || 3500;
 const jsonData = require('./users_example.json');
 
 app.use(express.json());
@@ -13,8 +13,8 @@ app.get('/all', (req, res) => {
     res.json(jsonData);
 });
 
-app.post('/validacion', (req, res) => {
-    console.log("req => ", req.body);
+app.post('/go-v3-dev/digital/webhooks/kyc', (req, res) => {
+    console.log("req prueba => ", req.body);
     res.json({response: req.body});
 })
 
